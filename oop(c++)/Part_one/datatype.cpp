2acc7333ff14a4cbeb7a8d7e0e7572619cb0de6f
josephmunyarukundo <147726123+josephmunyarukundo@users.noreply.github.com>
2025-02-21 10:46:30 +0200

    Add files via upload

---
+++ oop(c++)/Part_one/datatype.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 20;
+    double price = 19.99;
+    char grade = 'A';
+    string name = "Alice";
+
+    cout << "Age: " << age << endl;
+    cout << "Name: " << name << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Price: $" << price << endl;
+    return 0;
+}
---
+++ oop(c++)/Part_one/operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10, b = 10;
+    cout << "Sum: " << a + b << endl;
+    cout << "Difference: " << a - b << endl;
+    cout << "Product: " << a * b << endl;
+    cout << "quatient: " << a / b << endl;
+    cout << "remainder: " << a % b << endl;
+    return 0;
+}
